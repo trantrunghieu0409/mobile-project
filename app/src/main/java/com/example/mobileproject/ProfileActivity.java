bdd0264cf3fd75c80e2f00976a47a8d3b0eed834
lonelynight-dark <52921466+lonelynight-dark@users.noreply.github.com>
2022-02-22 12:14:52 +0700

    Profile Screen
    Code interface of the profile screen
    Border: profile border + text (smaller) border in layout

---
+++ app/src/main/java/com/example/mobileproject/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.example.mobileproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class ProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+
+    }
+}
