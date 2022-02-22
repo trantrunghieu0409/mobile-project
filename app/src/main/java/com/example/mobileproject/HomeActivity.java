85dd508f502389962a967f5ad5ee25f6cc5f6864
JrMasterno1 <56443232+JrMasterno1@users.noreply.github.com>
2022-02-22 11:52:03 +0700

    UI home screen

---
+++ app/src/main/java/com/example/mobileproject/HomeActivity.java
@@ -0,0 +1,21 @@
+package com.example.mobileproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.Nullable;
+
+public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);
+        Spinner spinner = (Spinner) findViewById(R.id.languages_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.languages_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0);
+    }
+}
