8f13641e75c4c4aaec19cb9ff66e2e83bebd2f65
Ho Van Duy <54297602+Catelt@users.noreply.github.com>
2022-02-27 11:46:30 +0700

    Answer Screen

---
+++ app/src/main/java/com/example/mobileproject/GameplayActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobileproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class GameplayActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameplay);
+    }
+}
