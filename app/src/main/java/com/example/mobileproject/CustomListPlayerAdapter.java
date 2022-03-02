6c9df27f3fcc7c804c121a65c4c0e0f1675b9dd2
Ho Van Duy <54297602+Catelt@users.noreply.github.com>
2022-03-02 16:02:46 +0700

    Add Items for list player

---
+++ app/src/main/java/com/example/mobileproject/CustomListPlayerAdapter.java
@@ -0,0 +1,49 @@
+package com.example.mobileproject;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class CustomListPlayerAdapter extends BaseAdapter {
+    Context context;
+    ArrayList<Player> list;
+    public CustomListPlayerAdapter(ArrayList<Player> listPlayer){
+        this.list = listPlayer;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return list.get(i).getPoint();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row;
+        if (convertView == null) {
+            row = View.inflate(parent.getContext(), R.layout.custom_list_player, null);
+        }
+        else row = convertView;
+        TextView name = (TextView) row.findViewById(R.id.name_player);
+        TextView point = (TextView) row.findViewById(R.id.point_player);
+        Player player = (Player) getItem(position);
+        name.setText(player.getName());
+        point.setText(player.getPoint() + "pts");
+        return row;
+    }
+
+}
--- app/src/main/java/com/example/mobileproject/GameplayActivity.java
+++ app/src/main/java/com/example/mobileproject/GameplayActivity.java
@@ -2,13 +2,49 @@ package com.example.mobileproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
+class Player {
+    private String name;
+    private int point;
+    public Player(String name, int point){
+        this.name = name;
+        this.point = point;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
 public class GameplayActivity extends Activity {
+    ArrayList<Player> list = new ArrayList<>();
+    ListView listViewPlayer;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        list.add(new Player("User 1", 10));
+        list.add(new Player("User 2", 5));
         setContentView(R.layout.activity_gameplay);
+        listViewPlayer = (ListView) findViewById(R.id.list_player);
+
+        CustomListPlayerAdapter adapter = new CustomListPlayerAdapter(list);
+        listViewPlayer.setAdapter(adapter);
     }
 }
