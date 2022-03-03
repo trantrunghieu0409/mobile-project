1eb79a160d1fec371f0ab221af8c449a7555d003
JrMasterno1 <56443232+JrMasterno1@users.noreply.github.com>
2022-03-03 16:53:04 +0700

    fix layout draw screen

--- app/src/main/java/com/example/mobileproject/HomeActivity.java
+++ app/src/main/java/com/example/mobileproject/HomeActivity.java
@@ -2,20 +2,38 @@ package com.example.mobileproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 
 public class HomeActivity extends Activity {
+    String[] languages = {"Vietnamese", "English"};
+    Button btnPlay;
+    Button btnCreateRoom;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnCreateRoom = (Button) findViewById(R.id.btnCreateRoom);
         Spinner spinner = (Spinner) findViewById(R.id.languages_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.languages_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, languages);
         spinner.setAdapter(adapter);
-        spinner.setSelection(0);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnCreateRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_createroom);
+
+            }
+        });
     }
 }
