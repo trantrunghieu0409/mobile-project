29406ebb1f7de96fb800bf72b8fd71cf3ef97061
DangThi <19127276@student.hcmus.edu.vn>
2022-03-07 17:41:07 +0700

    Update LoginActivity

--- app/src/main/java/com/example/mobileproject/LoginActivity.java
+++ app/src/main/java/com/example/mobileproject/LoginActivity.java
@@ -1,14 +1,32 @@
 package com.example.mobileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button loginBtn;
+    String email,password;
+    EditText emailEditText,passwordEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        emailEditText=(EditText)findViewById(R.id.editTextEmail);
+        passwordEditText=(EditText) findViewById(R.id.editTextPassword);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
