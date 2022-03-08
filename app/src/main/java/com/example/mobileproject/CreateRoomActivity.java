469985ae890a925849e79165d1353a41cded302f
JrMasterno1 <56443232+JrMasterno1@users.noreply.github.com>
2022-03-08 22:47:22 +0700

    func homescreen & createroom

---
+++ app/src/main/java/com/example/mobileproject/CreateRoomActivity.java
@@ -0,0 +1,71 @@
+package com.example.mobileproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+public class CreateRoomActivity extends Activity implements View.OnClickListener {
+    String[] numPlayers = {"5 players", "10 players", "15 players", "20 players"};
+    String[] maxPoints = {"120 Points", "200 Points", "300 Points", "400 Points"};
+    String[] topics = {"Animal", "Household", "Transportation"};
+    Integer[] topicImgs = {R.drawable.topic1,R.drawable.topic2,R.drawable.topic3};
+    Spinner numPlayerSpinner;
+    Spinner maxPointSpinner;
+    TextView btnBack;
+    ImageButton btnNext;
+    ImageButton btnPrev;
+    CircularImageView imgTopic;
+    int pos = 0;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createroom);
+
+        imgTopic = (CircularImageView) findViewById(R.id.topic);
+
+        btnNext = (ImageButton) findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(++pos >= topicImgs.length){
+                    pos = 0;
+                }
+                imgTopic.setImageResource(topicImgs[pos]);
+            }
+        });
+        btnPrev = (ImageButton) findViewById(R.id.btnPrev);
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(--pos < 0){
+                    pos = topicImgs.length - 1;
+                }
+                imgTopic.setImageResource(topicImgs[pos]);
+            }
+        });
+
+        numPlayerSpinner = (Spinner) findViewById(R.id.players_spinner);
+        ArrayAdapter<String> playerAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, numPlayers);
+        numPlayerSpinner.setAdapter(playerAdapter);
+
+        maxPointSpinner = (Spinner) findViewById(R.id.points_spinner);
+        ArrayAdapter<String> pointAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, maxPoints);
+        maxPointSpinner.setAdapter(pointAdapter);
+
+        btnBack = (TextView) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        super.onBackPressed();
+    }
+}
--- app/src/main/java/com/example/mobileproject/HomeActivity.java
+++ app/src/main/java/com/example/mobileproject/HomeActivity.java
@@ -1,18 +1,36 @@
 package com.example.mobileproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class HomeActivity extends Activity {
     String[] languages = {"Vietnamese", "English"};
+    Integer[] avatars = {R.drawable.avatar,R.drawable.avatar1, R.drawable.avatar2, R.drawable.avatar3, R.drawable.avatar_batman};
+
     Button btnPlay;
     Button btnCreateRoom;
+    ImageButton btnNext;
+    ImageButton btnPrev;
+    ImageView btnLogin;
+    CircularImageView avatar;
+    EditText edtName;
+
+    int pos = 0;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,17 +40,56 @@ public class HomeActivity extends Activity {
         Spinner spinner = (Spinner) findViewById(R.id.languages_spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, languages);
         spinner.setAdapter(adapter);
+        btnNext = (ImageButton) findViewById(R.id.btnNext);
+        btnPrev = (ImageButton) findViewById(R.id.btnPrev);
+        avatar = (CircularImageView) findViewById(R.id.circle_avatar);
+        edtName = (EditText) findViewById(R.id.edtName);
+        btnLogin = (ImageView) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //go to login here
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(++pos >= avatars.length){
+                    pos = 0;
+                }
+                avatar.setImageResource(avatars[pos]);
+            }
+        });
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(--pos < 0){
+                    pos = avatars.length -1;
+                }
+                avatar.setImageResource(avatars[pos]);
+            }
+        });
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(edtName.getText().length() == 0){
+                    // Pop up Error message
+                }
+                else {
+                    //go to play screen here
+                }
             }
         });
         btnCreateRoom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.activity_createroom);
-
+                Intent roomIntent = new Intent(HomeActivity.this, CreateRoomActivity.class);
+                roomIntent.putExtra("name", edtName.getText());
+                roomIntent.putExtra("avatar", avatars[pos]);
+                startActivityForResult(roomIntent, 0);
             }
         });
     }
