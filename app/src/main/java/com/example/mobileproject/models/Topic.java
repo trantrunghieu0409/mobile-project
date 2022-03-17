8390e6158917155e914e376fc29bafe69e802403
DangThi <19127276@student.hcmus.edu.vn>
2022-03-17 10:38:45 +0700

    add Topic class with toJson, fromJson method

--- app/src/main/java/com/example/mobileproject/models/Topic.java
+++ app/src/main/java/com/example/mobileproject/models/Topic.java
@@ -1,5 +1,9 @@
 package com.example.mobileproject.models;
 
+import com.google.gson.Gson;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -12,5 +16,22 @@ public class Topic {
     Topic(HashMap<String, Object> json){
         topicName = json.get("topicName").toString();
         vocabs = Arrays.asList((String[])json.get("vocabs"));
+    }
+
+    Topic(String topicName, List<String> vocabs)
+    {
+        this.topicName=topicName;
+        this.vocabs=vocabs;
+    }
+    public String toJson(){
+        Gson gson=new Gson();
+        String json= gson.toJson(new Topic(topicName,vocabs));
+        return json;
+
+    }
+    public Topic fromJson(String json){
+        Gson gson=new Gson();
+        Topic topic= gson.fromJson(json,Topic.class);
+        return topic;
     }
 }
