98f2575fd86256e902095eae014508ab6e58bf2b
lonelynight-dark <52921466+trantrunghieu0409@users.noreply.github.com>
2022-03-18 11:11:01 +0700

    add GlobalConstants for global constants
    Please add global constants here!

---
+++ app/src/main/java/com/example/mobileproject/constants/GlobalConstants.java
@@ -0,0 +1,10 @@
+package com.example.mobileproject.constants;
+
+public class GlobalConstants {
+    /*
+        Please declare global constants here.
+        Global constants should be considered:
+        time for a game, number of players in game, score system, .etc
+     */
+    public static final int TIME_FOR_A_GAME = 20; // 20 seconds
+}
