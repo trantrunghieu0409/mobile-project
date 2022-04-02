99e1b900c7f969bc99ee3630739553169e3f2546
lonelynight-dark <52921466+trantrunghieu0409@users.noreply.github.com>
2022-04-02 15:55:28 +0700

    add adapter for friends

--- app/src/main/java/com/example/mobileproject/adapter/CustomListFriendAdapter.java
+++ app/src/main/java/com/example/mobileproject/adapter/CustomListFriendAdapter.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.mobileproject.R;
+import com.example.mobileproject.models.Account;
 import com.example.mobileproject.models.Player;
 
 import java.util.ArrayList;
@@ -14,37 +18,60 @@ import java.util.ArrayList;
 
 public class CustomListFriendAdapter extends BaseAdapter {
     Context context;
-    ArrayList<Player> list;
-    public CustomListFriendAdapter(ArrayList<Player> listPlayer){
-        this.list = listPlayer;
+    TextView username, status;
+    ImageView avatar, iconStatus;
+    ImageButton btnUnfriend;
+
+    ArrayList<Account> friendList;
+
+    public CustomListFriendAdapter(ArrayList<Account> friendList, Context context){
+        this.friendList = friendList;
+        this.context = context;
     }
 
     @Override
     public int getCount() {
-        return list.size();
+        return friendList.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return list.get(i);
+        return friendList.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return list.get(i).getPoint();
+        return friendList.get(i).getPoint();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View row;
         if (convertView == null) {
-            row = View.inflate(parent.getContext(), R.layout.custom_list_player, null);
+            row = View.inflate(parent.getContext(), R.layout.custom_list_friend, null);
         }
-        else row = convertView;
-        TextView name = (TextView) row.findViewById(R.id.name_player);
-        TextView point = (TextView) row.findViewById(R.id.point_player);
-        Player player = (Player) getItem(position);
-        name.setText(player.getName());
+        else
+            row = convertView;
+
+        Account friend = friendList.get(position);
+
+        username = (TextView) row.findViewById(R.id.txtUsername);
+        avatar = (ImageView) row.findViewById(R.id.imgAvatar);
+        btnUnfriend = (ImageButton) row.findViewById(R.id.btnUnfriend);
+
+        iconStatus = (ImageView) row.findViewById(R.id.iconStatus);
+        status = (TextView) row.findViewById(R.id.txtStatus);
+
+        username.setText(friend.getName());
+        avatar.setImageResource(friend.getAvatar());
+
+        btnUnfriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         return row;
     }
 
