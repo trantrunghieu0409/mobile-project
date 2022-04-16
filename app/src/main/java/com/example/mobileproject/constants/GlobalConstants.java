85ed60af0fc66faf6074f5970a0656781e969526
JrMasterno1 <56443232+JrMasterno1@users.noreply.github.com>
2022-04-16 21:22:41 +0700

    add more words

--- app/src/main/java/com/example/mobileproject/constants/GlobalConstants.java
+++ app/src/main/java/com/example/mobileproject/constants/GlobalConstants.java
@@ -14,10 +14,10 @@ public class GlobalConstants {
         R.drawable.avatar1, R.drawable.avatar2, R.drawable.avatar3};
 
     public static final String[] topics = {"Animal", "Household", "Transportation"};
-    public static final String[] animals = {"cat", "dog", "mouse", "chicken", "lion"};
+    public static final String[] animals = {"cat", "dog", "mouse", "chicken", "lion", "tiger", "elephant", "monkey", "fox", "bird", "horse", "fish"};
     public static final String[] households = {"tool box", "box cutter", "duct tape",
-            "hammer", "step ladder", "level"};
-    public static final String[] transportations = {"car", "bike", "motorbike", "train",
+            "hammer", "step ladder", "level", "table", "chair", "pan", "fan", "bookshelf", "cupboard", "television", "refrigerator"};
+    public static final String[] transportations = {"car", "bike", "motorbike", "train", "subway", "road", "sign", "stop", "traffic light", "container",
             "plane", "truck"};
 
     public static final int[] nPlayers = {5, 10, 15, 20};
